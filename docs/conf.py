18dc9c6cc7a1e16c55458d0cfa0995e4f3395214
Mark Piper <mark.piper@colorado.edu>
2021-03-03 16:06:43 -0700

    Make docs (#3)
    * Create index page for docs
    
    * Remove unused files
    
    * Add API docs
    
    * Add sidebar info
    
    * Fix lack of jinja templating
    
    * Add a pair of demonstration notebooks
    
    * Add notebook examples to docs
    
    * Add Read the Docs config
    
    * Add requirements file for docs
    
    * Provide the correct name jfc

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import pymt_topography
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -96,6 +96,26 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "_static/powered-by-logo-header.png"
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    "index": [
+        "sidebarintro.html",
+        "links.html",
+        "sourcelink.html",
+        "searchbox.html",
+    ],
+    "**": [
+        "sidebarintro.html",
+        "links.html",
+        "sourcelink.html",
+        "searchbox.html",
+    ]
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------
